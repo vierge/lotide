d5b503b807f37e7d441bc9e13b420a5ab907ba1a
vierge <marancholia@gmail.com>
2020-01-06 17:59:17 +0000

    added assertEqual.js to the library

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  console.log(
+    actual === expected ? '✨: ' + actual + ' === ' + expected :
+      '🚨: ' + actual + ' !== ' + expected
+  );
+};
+
+//test
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Mara", "Mara");
+assertEqual(1, 2);
+assertEqual("Mara", 3);
