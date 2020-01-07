7fb73b8bf0c82701c49b6eaa0ed1757637c822e3
vierge <marancholia@gmail.com>
2020-01-07 17:47:16 +0000

    added eqArrays.js to repo. eqArrays can match non-arrays as well; it will check for array pairs on exec

---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const eqArrays = function(actual, expected) {
+  if (actual.constructor === Array && expected.constructor === Array) {
+    actual = actual.join();
+    expected = expected.join();
+  }
+
+  console.log(
+    actual === expected ? `✨: ${actual} === ${expected}` :
+      `🚨: ${actual} !== ${expected}`
+  );
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
