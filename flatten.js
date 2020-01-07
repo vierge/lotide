7ef8dc34d560b6e8bc840b45a7b0b61e3bcb3fa7
vierge <marancholia@gmail.com>
2020-01-07 20:32:53 +0000

    implemented flatten.js. NOTE: need to clean up other functions

--- flatten.js
+++ flatten.js
@@ -10,3 +10,11 @@ const eqArrays = function(actual, expected) {
   );
 };
 
+const flatten = function(array) {
+  let result = [];
+  for(element of array) {
+    Array.isArray(element) ? result.push(element.join()) :
+      result.push(element);
+  }
+return result;
+}
