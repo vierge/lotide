b14e0892e5a9e71455e904ec305d4a382517eb1d
vierge <marancholia@gmail.com>
2020-01-13 17:23:13 +0000

    refactored tailTest and middleTest to mocha/chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,24 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const middle  = require('../middle');
 
-console.log(assertEqual(middle([3, 4, 2, 5]), [4, 2]));
-console.log(assertEqual(middle([3, 4, 2]), [4]));
-console.log(assertEqual(middle([5, 10, 50, 100]), [10, 50]));
-console.log(assertEqual(middle([5, 10, 50]), [10]));
+describe('#middle', () => {
+  it('should return the middle index of an odd length array', () => {
+    assert.deepEqual(middle([3, 4, 2, 5]), [4, 2]);
+  });
+  it('should return the middle two indexes of an even numbered array', () => {
+    assert.deepEqual(middle([5, 10, 50, 100]), [10, 50]);
+  });
+}); 
+
+
+
+
+
+
+
+
+
+// console.log(assertEqual(middle([3, 4, 2, 5]), [4, 2]));
+// console.log(assertEqual(middle([3, 4, 2]), [4]));
+// console.log(assertEqual(middle([5, 10, 50, 100]), [10, 50]));
+// console.log(assertEqual(middle([5, 10, 50]), [10]));
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,18 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-assertEqual(tail([1, 2, 3]), [2, 3]);
-assertEqual(tail([42]), []);
-assertEqual(tail(['this', 'is', 'a', 'test']), ['is', 'a', 'test']);
+
+describe('#tail', () => {
+  it('should return an array without the head', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+    assert.deepEqual(tail(['this', 'is', 'a', 'test']), ['is', 'a', 'test']);
+  });
+  it('should return an empty array if the input has only one index', () => {
+    assert.deepEqual(tail([42]), []);
+  });
+});
+
+
+// assertEqual(tail([1, 2, 3]), [2, 3]);
+// assertEqual(tail([42]), []);
+// assertEqual(tail(['this', 'is', 'a', 'test']), ['is', 'a', 'test']);
