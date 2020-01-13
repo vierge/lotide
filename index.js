f89b36184fa3b9e1f33e354ab13e4f20622ad6d8
vierge <marancholia@gmail.com>
2020-01-13 17:36:42 +0000

    added index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertEqual: assertEqual
+};
